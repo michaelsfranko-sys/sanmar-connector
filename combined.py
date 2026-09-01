eb3a64b4946fef169d5ee7feb6cbff18c891c686
michaelsfranko-sys <michael.s.franko@gmail.com>
2026-08-31 20:08:29 -0400

    Include SanMar draft import routes

--- combined.py
+++ combined.py
@@ -1,4 +1,6 @@
 from main import app
 from shopify_connector import router as shopify_router
+from shopify_import import router as shopify_import_router
 
 app.include_router(shopify_router)
+app.include_router(shopify_import_router)
