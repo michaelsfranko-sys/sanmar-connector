bc282fc00c8783912af73a5191a2cf68e1b7c9f5
michaelsfranko-sys <michael.s.franko@gmail.com>
2026-08-31 20:39:03 -0400

    Expose Shopify draft update endpoint

--- combined.py
+++ combined.py
@@ -1,6 +1,8 @@
 from main import app
 from shopify_connector import router as shopify_router
 from shopify_import import router as shopify_import_router
+from shopify_update import router as shopify_update_router
 
 app.include_router(shopify_router)
 app.include_router(shopify_import_router)
+app.include_router(shopify_update_router)
