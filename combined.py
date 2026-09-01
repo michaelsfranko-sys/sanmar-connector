5fa21c1dc2dc96749917ee98fe6ea2a17b3868dd
michaelsfranko-sys <michael.s.franko@gmail.com>
2026-08-31 21:16:31 -0400

    Include Shopify media and mockup routes

--- combined.py
+++ combined.py
@@ -2,7 +2,9 @@ from main import app
 from shopify_connector import router as shopify_router
 from shopify_import import router as shopify_import_router
 from shopify_update import router as shopify_update_router
+from shopify_media import router as shopify_media_router
 
 app.include_router(shopify_router)
 app.include_router(shopify_import_router)
 app.include_router(shopify_update_router)
+app.include_router(shopify_media_router)
