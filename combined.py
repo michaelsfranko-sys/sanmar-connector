eb72177d4f284d561456fd4da54e8005437f36cb
michaelsfranko-sys <michael.s.franko@gmail.com>
2026-09-02 21:08:35 -0400

    Wire multi-store Shopify reporting router

--- combined.py
+++ combined.py
@@ -3,8 +3,10 @@ from shopify_connector import router as shopify_router
 from shopify_import import router as shopify_import_router
 from shopify_update import router as shopify_update_router
 from shopify_media import router as shopify_media_router
+from shopify_reports import router as shopify_reports_router
 
 app.include_router(shopify_router)
 app.include_router(shopify_import_router)
 app.include_router(shopify_update_router)
 app.include_router(shopify_media_router)
+app.include_router(shopify_reports_router)
